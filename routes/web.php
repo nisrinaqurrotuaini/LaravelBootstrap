f78ab3ec6101893b135a18aaae9cb31461aad7c7
Nisrina <nisrinaqurrotuaini06@gmail.com>
2024-10-09 15:23:35 +0700

    menambah file baru

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/content', function () {
+    return view('content');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::post('/contact-submit', function () {
+    return redirect('/contact')->with('success', 'Thank you for your message! We will get back to you soon.');
+});
